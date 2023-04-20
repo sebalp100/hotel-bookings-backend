27d4c75483475c0bbe478e3f09dc6a991e259c34
SimonGideon <simongideon918@gmail.com>
2023-04-20 20:27:31 +0300

    Fix room and user models and create reservations validations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,12 @@
+Resolving dependencies...
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :room
+  validate :unique_room_date
+
+  def unique_room_date
+    if Reservation.where(room_id: room_id, date: date).exists?
+      errors.add(:base, "This room is already reserved for this date")
+    end
+  end
 end
