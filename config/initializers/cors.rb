fb8f7f4d6324534cf8bd8eb21c92b04a9615f401
sebalp100 <sebalp100@gmail.com>
2023-05-15 19:25:12 -0300

    Update CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -11,6 +11,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource '*', 
       headers: :any, 
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+      credentials: true,
+      expose: ['Access-Control-Allow-Headers']
   end
 end
