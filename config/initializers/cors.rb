5bf19368c069d743b6c1a84e37d9283745c24e5c
sebalp100 <sebalp100@gmail.com>
2023-05-15 18:04:01 -0300

    Remove port from cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://hotel-bookings.onrender.com:80'
+    origins 'https://hotel-bookings.onrender.com'
     resource '*', 
       headers: :any, 
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
