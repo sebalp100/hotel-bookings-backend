5ac57701e70317b0c2d5e3b70a374f8e440c49d6
sebalp100 <sebalp100@gmail.com>
2023-05-15 19:53:19 -0300

    Set credentials to false

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,6 +10,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins 'https://hotel-bookings.onrender.com'
     resource '*', 
       headers: :any, 
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: false
   end
 end
