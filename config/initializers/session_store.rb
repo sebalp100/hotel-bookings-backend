d2d65a928b00567d88eb2a4f1d69593dee488b65
sebalp100 <sebalp100@gmail.com>
2023-05-15 20:03:37 -0300

    Fix session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: '_your_app_session', domain: :all, tld_length: 2, secure: Rails.env.production?, same_site: :none
+Rails.application.config.session_store :cookie_store, key: '_hotel-booking', domain: :all, tld_length: 2, secure: Rails.env.production?, same_site: :none
