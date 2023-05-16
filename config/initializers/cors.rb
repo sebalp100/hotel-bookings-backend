f125f48184b032ff419d25b6089b2449920f0dd3
sebalp100 <sebalp100@gmail.com>
2023-05-15 22:56:37 -0300

    Update origin to ip

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://hotel-bookings-frontend-r1held8dq-sebalp100.vercel.app/'
+    origins 'http://76.76.21.9'
     resource '*', 
       headers: :any, 
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
