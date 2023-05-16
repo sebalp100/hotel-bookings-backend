a0b8b806775314b0717eaab2f8f6a880b0eae08b
sebalp100 <sebalp100@gmail.com>
2023-05-15 22:46:46 -0300

    Update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://hotel-bookings-frontend.vercel.app/'
+    origins 'https://hotel-bookings-frontend-r1held8dq-sebalp100.vercel.app/'
     resource '*', 
       headers: :any, 
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
