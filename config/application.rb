faa97f6bd9bf889c5a56b73ff3edc369d761050a
sebalp100 <sebalp100@gmail.com>
2023-05-15 23:02:11 -0300

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,10 @@ module HotelBookingsBackend
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+    config.action_dispatch.default_headers = {
+    'Access-Control-Allow-Origin' => 'https://hotel-bookings-frontend-r1held8dq-sebalp100.vercel.app',
+    'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+    }
     config.api_only = true
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore, key: '_hotel-booking'
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://76.76.21.9'
+    origins 'https://hotel-bookings-frontend-r1held8dq-sebalp100.vercel.app/'
     resource '*', 
       headers: :any, 
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
