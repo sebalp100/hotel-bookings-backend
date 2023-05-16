ce0122e7e710c69fe3b047081da392e0177edc29
sebalp100 <sebalp100@gmail.com>
2023-05-15 22:41:26 -0300

    Edit cors origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://hotel-bookings.onrender.com'
+    origins 'https://hotel-bookings-frontend.vercel.app/'
     resource '*', 
       headers: :any, 
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
