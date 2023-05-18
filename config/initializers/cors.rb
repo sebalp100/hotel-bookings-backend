3071ce870ec3dc47b5b2feb270efa11456cb0c26
sebalp100 <sebalp100@gmail.com>
2023-05-17 23:38:21 -0300

    Edit cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3001'
+    origins 'https://hotel-bookings-frontend-pgfmtudo4-sebalp100.vercel.app/'
     resource '*', 
       headers: :any, 
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
