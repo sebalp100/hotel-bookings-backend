43518a33daec0adebef8b925c54096e6ef1ce90e
sebalp100 <sebalp100@gmail.com>
2023-05-19 14:42:09 -0300

    Update session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: '_hotel-booking', domain: 'https://rails-production-68eb.up.railway.app/', same_site: :none, secure: true 
+  Rails.application.config.session_store :cookie_store, key: '_hotel-booking', domain: 'https://rails-production-68eb.up.railway.app', same_site: :none, secure: true 
 else
   Rails.application.config.session_store :cookie_store, key: '_hotel-booking'
 end
